9b679a6b92250d91f615025006e9900ff4ba580b
Vahora's Laptop <amrinvahora21898@gmail.com>
2021-02-02 21:52:19 -0600

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import Welcome from "./components/welcome/Welcome";
+import Clock from "./components/clock/Clock";
+import Contact from "./components/contact/Contact";
+import Navigation from "./components/navigation/Navigation";
+import Error from "./components/404error/Error";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navigation />
+        <switch>
+          <Route
+            exact
+            path="/"
+            render={(props) => <Welcome {...props} name="Amrin" />}
+          />
+          <Route path="/clock" component={Clock} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/welcome/:name" component={Welcome} />
+          <Route path="/:name" component={Error} />
+        </switch>
+      </div>
+    );
+  }
+}
+
+export default App;
